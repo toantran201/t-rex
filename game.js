843121c2b0bf54c4004db419256404cb74021935
Duc Toan Tran <toantd@creativeforce.io>
2023-01-08 11:38:49 +0700

    Hone

---
+++ game.js
@@ -0,0 +1,7 @@
+function findMatch() {
+  const homeBlock = document.getElementById('home')
+  const gameBlock = document.getElementById('main-frame-error')
+
+  homeBlock.style.display = 'none'
+  gameBlock.style.visibility = 'visible'
+}
